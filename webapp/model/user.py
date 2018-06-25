47c4de4cb7de2f0b1b5e3e2daff761efffe07294
Paul Soule <paul@souletech.com>
2018-06-25 08:19:20 +0100

    Workaround for the div % issues on header

--- webapp/model/user.py
+++ webapp/model/user.py
@@ -26,17 +26,3 @@ class User:
     def find_by_id(id):
         return User(id)
 
-# this is what it might look like if we were a real application
-# user=User.query.filter_by(email=email).first()
-# if not user:
-#     # Create the user. Try and use their name returned by Google,
-#     # but if it is not set, split the email address at the @.
-#     nickname = username
-#     if nickname is None or nickname == "":
-#         nickname = email.split('@')[0]
-
-#     # We can do more work here to ensure a unique nickname, if you
-#     # require that.
-#     user=User(nickname=nickname, email=email)
-#     db.session.add(user)
-#     db.session.commit()
