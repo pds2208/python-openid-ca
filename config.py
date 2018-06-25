835ee25642ac3b73b96399c0fed436698f5bba9c
Paul Soule <paul@souletech.com>
2018-06-25 08:04:08 +0100

    Workaround for the div % issues on header

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
-client_id = '77d9c32d-3a02-4cb4-8c69-33946505d553'
-client_secret = 'f9feada3-973f-43c1-9737-70636d576130'
+client_id = 'py-client'
+client_secret = 'py-secret'
 discovery_endpoint = 'https://localhost:8443/.well-known/openid-configuration'
 
 # set the environment CURL_CA_BUNDLE=
